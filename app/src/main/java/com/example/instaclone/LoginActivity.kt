973f0f19f5809b72945d73f79bf940d178847656
andy <alabrada0810@gmail.com>
2022-03-26 02:16:12 -0400

    part 1 complete

--- app/src/main/java/com/example/instaclone/LoginActivity.kt
+++ app/src/main/java/com/example/instaclone/LoginActivity.kt
@@ -1,40 +1,76 @@
 package com.example.instaclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.parse.ParseObject
+import com.parse.ParseUser
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        val firstObject = ParseObject("FirstClass")
-        firstObject.put("message","Hey ! First message from android. Parse is now connected")
-        firstObject.saveInBackground {
-            if (it != null){
-                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
-            }else{
-                Log.d("MainActivity","Object saved.")
-            }
+
+        if (ParseUser.getCurrentUser() != null){
+            goToMainActivity()
+        }
+
+        findViewById<Button>(R.id.login_btn).setOnClickListener {
+            val username = findViewById<EditText>(R.id.et_username).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+            loginUser(username, password)
+            Log.i("LoginBtn", "btn pressed")
+        }
+
+        findViewById<Button>(R.id.signupBtn).setOnClickListener {
+            val username = findViewById<EditText>(R.id.et_username).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+            signUpUser(username, password)
+            Log.i("LoginBtn", "btn pressed")
         }
     }
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_login)
-//
-//        findViewById<Button>(R.id.login_btn).setOnClickListener {
-//            val username = findViewById<EditText>(R.id.et_username).text.toString()
-//            val password = findViewById<EditText>(R.id.et_password).text.toString()
-//            loginUser(username, password)
-//            Log.i("LoginBtn", "btn pressed")
-//        }
-//    }
-//
-//    private fun loginUser(username: String, password: String) {
-//
-//    }
+
+    private fun signUpUser(username: String, password: String){
+
+        val user = ParseUser()
+
+        user.setUsername(username)
+        user.setPassword(password)
+
+        user.signUpInBackground { e ->
+            if (e == null) {
+
+            } else {
+                e.printStackTrace()
+            }
+        }
+
+    }
+
+    private fun loginUser(username: String, password: String) {
+        ParseUser.logInInBackground(username,password, ({ user, e ->
+            if (user != null) {
+                Log.i(TAG, "Successfully logged in")
+            }
+            else {
+                e.printStackTrace()
+                Toast.makeText(this, "Error logging in", Toast.LENGTH_SHORT).show()
+            }
+        }))
+    }
+
+    private fun goToMainActivity() {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    companion object {
+        const val TAG = "LoginActivity"
+    }
 }
 
